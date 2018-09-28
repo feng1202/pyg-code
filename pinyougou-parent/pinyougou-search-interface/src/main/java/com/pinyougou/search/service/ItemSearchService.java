3e50a42872df8f462d5e07015dc8ba485afd5ac9
feng <123456@uu.com>
2018-09-28 15:58:16 +0800

    pyg第九天代码

---
+++ pinyougou-parent/pinyougou-search-interface/src/main/java/com/pinyougou/search/service/ItemSearchService.java
@@ -0,0 +1,17 @@
+package com.pinyougou.search.service;
+
+import java.util.Map;
+
+/**
+ * 搜索服务接口
+ * @author Administrator
+ *
+ */
+public interface ItemSearchService {
+	
+	
+	public Map search(Map searchMap);
+		
+	
+
+}
